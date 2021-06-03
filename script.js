e11d8d77af93bf921b6a52da097294115bef1978
HannahJT <>
2021-06-03 13:03:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// calculates the square of whatever number you add into the input box
+let number = 0
+let sentence = 'hello, this is the number 3.'
+
+document.getElementById('button').addEventListener('click', multiplication)
+
+function multiplication () {
+  number = document.getElementById('input').value
+
+  number = parseInt(number)
+
+  number = number * number
+
+  alert(number)
+}
